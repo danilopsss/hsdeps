f9b0c6a38752033325e0f6cd8a8013842a5d2d92
Danilo Paes <danilo.paes@pm.me>
2025-05-21 00:23:00 +0200

    adding interfaces for Redis DB

---
+++ pkg/db/client.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Redis interface {
+	DBClient()
+}
+
+type DB interface {
+	Redis
+}
+
+func GetCredentials() *redis.Options {
+	host := os.Getenv("DB_HOST")
+	pass := os.Getenv("DB_PASS")
+	port := os.Getenv("DB_PORT")
+	db, _ := strconv.Atoi(port)
+	return &redis.Options{
+		Addr:     fmt.Sprintf("%s:6379", host),
+		Password: pass,
+		DB:       db,
+	}
+}
+
+func DBClient() *redis.Client {
+	credentials := GetCredentials()
+	return redis.NewClient(credentials)
+}
