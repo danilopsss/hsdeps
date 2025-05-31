34002dc0ab2efd89d9a69bfad210afe74e661e5e
Danilo Paes <danilo.paes@pm.me>
2025-05-31 18:17:44 +0200

    adding user to db connection

--- pkg/db/client.go
+++ pkg/db/client.go
@@ -18,10 +18,12 @@ type DB interface {
 
 func GetCredentials() *redis.Options {
 	host := os.Getenv("DB_HOST")
+	username := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASS")
 	port := os.Getenv("DB_PORT")
 	db, _ := strconv.Atoi(port)
 	return &redis.Options{
+		Username: username,
 		Addr:     fmt.Sprintf("%s:6379", host),
 		Password: pass,
 		DB:       db,
