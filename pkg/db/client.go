bbe1b593dc8d288c8b7a43f8425546e9f37de9b5
Danilo Paes <danilo.paes@pm.me>
2025-06-08 13:47:00 +0200

    updating the clients

--- pkg/db/client.go
+++ pkg/db/client.go
@@ -1,37 +1,30 @@
 package db
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-
 	"github.com/redis/go-redis/v9"
 )
 
-type Redis interface {
-	DBClient()
+type RedisCreds struct {
+	host string
+	user string
+	pass string
+	db   int
 }
 
-type DB interface {
-	Redis
+type Clients interface {
+	GetRedisClient(creds RedisCreds) (*redis.Client, error)
 }
 
-func GetCredentials() *redis.Options {
-	host := os.Getenv("DB_HOST")
-	username := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASS")
-	db, _ := strconv.Atoi(os.Getenv("DB_NUMBER"))
-	log.Printf("Creating connection: %s:6379", host)
-	return &redis.Options{
-		Username: username,
-		Addr:     fmt.Sprintf("%s:6379", host),
-		Password: pass,
-		DB:       db,
+type Databases struct {
+	db Clients
+}
+
+func GetRedisClient(r RedisCreds) *redis.Client {
+	redisOpt := &redis.Options{
+		Addr:     r.host,
+		Username: r.user,
+		Password: r.pass,
+		DB:       r.db,
 	}
-}
-
-func DBClient() *redis.Client {
-	credentials := GetCredentials()
-	return redis.NewClient(credentials)
+	return redis.NewClient(redisOpt)
 }
